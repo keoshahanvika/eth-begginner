156a1ecd3de746bb3f257f08d317ea2b3b836823
keoshahanvika <148708773+keoshahanvika@users.noreply.github.com>
2023-10-22 23:06:57 +0530

    Update customtoken.sol

--- customtoken.sol
+++ customtoken.sol
@@ -1,1 +1,23 @@
+contract MyToken {
 
+    //public varibales
+    string public tokenName = "SOLANA";
+    string public tokenSymbol = "SOL";
+    uint public totalSupply = 0;
+    
+    //mapping variable
+    mapping(address => uint) public tokenBalances;
+
+    //Mint Token Function
+    function mintT(address _address, uint _amount) public {
+        totalSupply += _amount;
+        tokenBalances[_address] += _amount;
+    }
+
+    //Burn token functions
+    function burnT(address _address, uint _amount) public {
+        require(tokenBalances[_address] >= _amount, "Insufficient balance");
+        totalSupply -= _amount;
+        tokenBalances[_address] -= _amount;
+    }
+}
